fcb17b1bc255744315ba473c58b25c3b5343de19
abdulkaharr <60811105+abdulkaharr@users.noreply.github.com>
2021-04-08 18:30:26 +0300

    Data Time Picker

---
+++ DateTimePickerOrnek/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace DateTimePickerOrnek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtpbaslangic = new System.Windows.Forms.DateTimePicker();
+            this.dtpbitis = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnfark = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblDakika = new System.Windows.Forms.Label();
+            this.lblSaat = new System.Windows.Forms.Label();
+            this.lblGün = new System.Windows.Forms.Label();
+            this.lblAy = new System.Windows.Forms.Label();
+            this.lblYıl = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dtpbaslangic
+            // 
+            this.dtpbaslangic.Location = new System.Drawing.Point(12, 27);
+            this.dtpbaslangic.Name = "dtpbaslangic";
+            this.dtpbaslangic.Size = new System.Drawing.Size(200, 20);
+            this.dtpbaslangic.TabIndex = 0;
+            // 
+            // dtpbitis
+            // 
+            this.dtpbitis.Location = new System.Drawing.Point(12, 81);
+            this.dtpbitis.Name = "dtpbitis";
+            this.dtpbitis.Size = new System.Drawing.Size(200, 20);
+            this.dtpbitis.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Cornsilk;
+            this.label1.Location = new System.Drawing.Point(13, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Başlangıç Tarihi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Cornsilk;
+            this.label2.Location = new System.Drawing.Point(13, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Bitiş Tarihi";
+            // 
+            // btnfark
+            // 
+            this.btnfark.ForeColor = System.Drawing.Color.Red;
+            this.btnfark.Location = new System.Drawing.Point(12, 107);
+            this.btnfark.Name = "btnfark";
+            this.btnfark.Size = new System.Drawing.Size(83, 35);
+            this.btnfark.TabIndex = 3;
+            this.btnfark.Text = "Fark Hesapla";
+            this.btnfark.UseVisualStyleBackColor = true;
+            this.btnfark.Click += new System.EventHandler(this.btnfark_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.lblDakika);
+            this.groupBox1.Controls.Add(this.lblSaat);
+            this.groupBox1.Controls.Add(this.lblGün);
+            this.groupBox1.Controls.Add(this.lblAy);
+            this.groupBox1.Controls.Add(this.lblYıl);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.ForeColor = System.Drawing.Color.Cornsilk;
+            this.groupBox1.Location = new System.Drawing.Point(12, 162);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(252, 90);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tarihler Arası Fark";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label7.Location = new System.Drawing.Point(189, 29);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 13);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Dakika";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label6.Location = new System.Drawing.Point(138, 29);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Saat";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(102, 29);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Gün";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(62, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Ay";
+            // 
+            // lblDakika
+            // 
+            this.lblDakika.AutoSize = true;
+            this.lblDakika.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDakika.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblDakika.Location = new System.Drawing.Point(189, 55);
+            this.lblDakika.Name = "lblDakika";
+            this.lblDakika.Size = new System.Drawing.Size(21, 13);
+            this.lblDakika.TabIndex = 2;
+            this.lblDakika.Text = "00";
+            // 
+            // lblSaat
+            // 
+            this.lblSaat.AutoSize = true;
+            this.lblSaat.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSaat.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblSaat.Location = new System.Drawing.Point(138, 55);
+            this.lblSaat.Name = "lblSaat";
+            this.lblSaat.Size = new System.Drawing.Size(39, 13);
+            this.lblSaat.TabIndex = 2;
+            this.lblSaat.Text = "00:00";
+            // 
+            // lblGün
+            // 
+            this.lblGün.AutoSize = true;
+            this.lblGün.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblGün.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblGün.Location = new System.Drawing.Point(102, 55);
+            this.lblGün.Name = "lblGün";
+            this.lblGün.Size = new System.Drawing.Size(21, 13);
+            this.lblGün.TabIndex = 2;
+            this.lblGün.Text = "00";
+            // 
+            // lblAy
+            // 
+            this.lblAy.AutoSize = true;
+            this.lblAy.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblAy.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblAy.Location = new System.Drawing.Point(62, 55);
+            this.lblAy.Name = "lblAy";
+            this.lblAy.Size = new System.Drawing.Size(21, 13);
+            this.lblAy.TabIndex = 2;
+            this.lblAy.Text = "00";
+            // 
+            // lblYıl
+            // 
+            this.lblYıl.AutoSize = true;
+            this.lblYıl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblYıl.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblYıl.Location = new System.Drawing.Point(15, 55);
+            this.lblYıl.Name = "lblYıl";
+            this.lblYıl.Size = new System.Drawing.Size(46, 13);
+            this.lblYıl.TabIndex = 2;
+            this.lblYıl.Text = "00.000";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(15, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Yıl";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(269, 266);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnfark);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtpbitis);
+            this.Controls.Add(this.dtpbaslangic);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hesaplayıcı";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtpbaslangic;
+        private System.Windows.Forms.DateTimePicker dtpbitis;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnfark;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblDakika;
+        private System.Windows.Forms.Label lblSaat;
+        private System.Windows.Forms.Label lblGün;
+        private System.Windows.Forms.Label lblAy;
+        private System.Windows.Forms.Label lblYıl;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ DateTimePickerOrnek/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DateTimePickerOrnek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnfark_Click(object sender, EventArgs e)
+        {
+            DateTime baslangic = Convert.ToDateTime(dtpbaslangic.Text); // Tarih ile işlem yapacağamız için DateTime Kullanıyoruz 
+            DateTime bitis = Convert.ToDateTime(dtpbitis.Text);
+
+            TimeSpan sonuc = bitis - baslangic; // TimeSpan Sınıfı İki Tarih arası farkı hesaplar
+
+            lblYıl.Text = Math.Round(sonuc.TotalDays / 365, 4).ToString(); // total günü 365,4 bölüp math.Round ile yuvarlayıp lblYıl a aktardık 
+            lblAy.Text = Math.Round(sonuc.TotalDays / 30).ToString();
+            lblGün.Text = Math.Round(sonuc.TotalDays,2).ToString();
+            lblSaat.Text = Math.Round(sonuc.TotalHours).ToString();
+            lblDakika.Text = Math.Round(sonuc.TotalMinutes).ToString();
+
+    }
+    }
+}
